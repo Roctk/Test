7b7129795d07e83c82d7ca6285359d2fe73cf3e7
Roctik <roctik1996@gmail.com>
2018-12-05 17:41:45 +0200

    1.1

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,28 @@
+import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main{
+
     public static void main(String[] args) {
+        int check;
+        int num;
         Scanner in = new Scanner(System.in);
-        Set arr = new Set(13);
-        for (int i=0;i<13;i++)
-            arr.add(in.next());
+        Set arr = new Set<Integer>();
+        for (int i=0;i<10;i++)
+        {
+            num=new Random().nextInt(10);
+            arr.add(num);
+        }
+        for (int i=0;i<10;i++)
+        {
+            num=new Random().nextInt(10);
+            System.out.print(num+"____");
+            arr.contains(num);
+        }
+
+
+        System.out.println(Arrays.asList(arr.get()));
+        arr.add(in.next());//11
     }
 }
--- src/Set.java
+++ src/Set.java
@@ -4,46 +4,18 @@ public class Set<T> implements LimitedSet<T> {
 
     private int count;
     private T[] collection;
-    private Map map = new HashMap<T, T>();
+    private final static int size = 10;
 
     @SuppressWarnings("unchecked")
-    public Set(int size){
+    public Set(){
         collection = (T []) new Object [size];
         count = 0;
     }
     @Override
     public void add(final T t) {
-        if(count>=10){
-            for (T string : lst) {
-                if(map.keySet().contains(string)) {
-                    map.put(string,Integer.parseInt(map.get(string).toString())+1);
-                } else {
-                    map.put(string, 1);
-                }
-            }
-            Iterator<Integer> iter = map.values().iterator();
-            int temp = count;
-            while (iter.hasNext()) {
-                int max = iter.next();
-                if (max < temp) {
-                    temp = max;
-                }
-            }
-            remove((T) map.get(temp));
-            System.out.println(temp);
-            System.out.println(map);
-        }
-        if (count==9){
-            collection[count] = t;
-            count++;
-            contains(t);
-        }
-        else{
-            collection[count] = t;
-            count++;
-            contains(t);
-            contains(t);
-        }
+        collection[count] = t;
+        count++;
+        System.out.println(t);
 
     }
 
@@ -62,18 +34,21 @@ public class Set<T> implements LimitedSet<T> {
         return check;
     }
 
-    List<T> lst = new ArrayList<T>();
     @Override
     public boolean contains(final T t) {
-        lst.add(t);
         for (int i = 0; i < count; i++)
         {
             if (collection[i].equals(t))
             {
+                System.out.println(true);
                 return true;
             }
         }
 
+        System.out.println(false);
         return false;
+    }
+    public T[] get(){
+        return collection;
     }
 }
