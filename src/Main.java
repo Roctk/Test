3784e95d71683ee1a1b9ddea17bda4edd81234d4
Roctik <roctik1996@gmail.com>
2018-12-06 13:32:38 +0200

    fix

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,7 @@ public class Main{
             System.out.print(num +" ");
         }
         System.out.println();
-        for (int i=0;i<10;i++)
+        for (int i=0;i<1000;i++)
         {
             num=new Random().nextInt(20);
             arr.contains(num);
@@ -26,13 +26,6 @@ public class Main{
 
         arr.get();
         arr.add(in.next());//11
-        arr.get();
-        arr.add(in.next());//11
-        arr.get();
-        arr.add(in.next());//11
-        arr.get();
-        arr.add(in.next());//11
-        arr.get();
 
 
     }
